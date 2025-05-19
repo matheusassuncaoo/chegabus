4f00ec8c249fac7342791fc459933a0ed80cbfba
Matheus Assunção da Cunha <106777600+matheusassuncaoo@users.noreply.github.com>
2025-05-19 19:33:38 -0400

    v1.0.1
    - refatoração da imagens

--- Assets/js/dashboard.js
+++ Assets/js/dashboard.js
@@ -3,15 +3,15 @@ feather.replace();
 const popularRoutes = [
     { destination: "Cuiabá, MT → Sinop, MT", image: "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcQ84rKF_82HHLKusSWRkLwQkyRn46WrlIUW5fJESRi-RuZp1QiLy9jBVxhq0SmvRxgGibFq2CJ-BXehMTKaYd5LeDQgSYOQ_jukTfEgQg", discount: "-25%" },
     { destination: "Cuiabá, MT → Primavera do Leste, MT", image: "https://lh3.googleusercontent.com/p/AF1QipNskjxkS1JMO24x0g6JJwwDnyKbfFr00RWdVB-O=w594-h343-n-k-no", discount: "-20%" },
-    { destination: "Nobres, MT → Sinop, MT", image: "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn9GcQ84rKF_82HHLKusSWRkLwQkyRn46WrlIUW5fJESRi-RuZp1QiLy9jBVxhq0SmvRxgGibFq2CJ-BXehMTKaYd5LeDQgSYOQ_jukTfEgQg", discount: "-15%" },
-    { destination: "Cuiabá, MT → Alto Araguaia, MT", image: "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn9GcRZ5ayhTBpWDiJYZtwmFzw8h4rZMp5IxEy-E-PBbFiVv4-5JAoY70GZSURg5_-nIdDp7gA-QvDl10Vho4pbsVfX5KfoEXvgY6sm9M2L3w", discount: "-85%" }
+    { destination: "Nobres, MT → Sinop, MT", image: "https://portalmatogrosso.com.br/wp-content/uploads/2024/03/sinop-centro.jpeg", discount: "-15%" },
+    { destination: "Cuiabá, MT → Alto Araguaia, MT", image: "https://cdn.semana7.com.br/storage/webdisco/2021/10/26/1200x900/e2fb00c22b857d69a1bed9786c204747.jpg", discount: "-85%" }
 ];
 
 const recentRoutes = [
     { destination: "Cuiabá, MT → Rio de Janeiro, RJ", image: "https://images.unsplash.com/photo-1483729558449-99ef09a8c325", discount: "-25%" },
     { destination: "Cuiabá, MT → São Paulo, SP", image: "https://lh3.googleusercontent.com/p/AF1QipMGIvyxHIHM6WzEEpOvqyVXatJC6W7QklJ5smMm=w594-h343-n-k-no", discount: "-25%" },
     { destination: "Florianópolis, SC → São Paulo, SP", image: "https://lh3.googleusercontent.com/p/AF1QipMGIvyxHIHM6WzEEpOvqyVXatJC6W7QklJ5smMm=w594-h343-n-k-no", discount: "-25%" },
-    { destination: "Salvador, BA → Manaus, AM", image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn9GcTR09PDgyiOVLbrU6QqpavE0kR6QBbcHYWNHj-yEwuCxJgzvO2wPbMd6lgawapE6iTsMoGFNXQfaUPwSeADrYAk3VshJ8Sx6CNjVvT-qQ", discount: "-85%" }
+    { destination: "Salvador, BA → Manaus, AM", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Sx0RymxmpIt6v7Z4uC7xfiuFVC01t-26hA&s", discount: "-85%" }
 ];
 
 function renderRoutes(containerId, routes) {
